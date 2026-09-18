986f1208549a7179333d7ac096c657e972f30acd
Kennedy Buchichi Masinde <143694221+Buuchezo@users.noreply.github.com>
2026-09-18 23:21:46 +0200

    updating the withdrawal implementations

--- src/main/java/com/buuchezo/transactionservice/dto/TransactionRequest.java
+++ src/main/java/com/buuchezo/transactionservice/dto/TransactionRequest.java
@@ -1,15 +1,12 @@
 package com.buuchezo.transactionservice.dto;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
-
 
 @Getter
 @Setter
@@ -22,11 +19,13 @@ public class TransactionRequest {
 
     private String fromAccountNumber;
 
-    @NotBlank(message = "Destination account number cannot be blank")
     private String toAccountNumber;
 
     @NotNull(message = "Amount is required")
-    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
+    @DecimalMin(
+            value = "0.01",
+            message = "Amount must be greater than 0"
+    )
     private BigDecimal amount;
 
     private String description;
